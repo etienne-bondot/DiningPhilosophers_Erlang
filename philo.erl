6330f066748d97b31981f7bc0a486e9113a02e91
Etienne BONDOT <etiennebondot@gmail.com>
2016-03-08 16:07:29 +0000

    Update and clean

--- philo.erl
+++ philo.erl
@@ -35,7 +35,6 @@ college() ->
     % spawns a report process, five fork processes and five philosopher processes.
     % It will need to pass the PIDs of the forks to the philosophers, and PID of the
     % reporting process to all forks and philosophers.
-    random:seed(erlang:timestamp()),
     spawn_report(),
     spawn_pair().
 
@@ -53,15 +52,15 @@ spawn_report() ->
 % philosopher to the next (thus beginning his right fork) and spawn only a
 % new left fork, and so on until we reach the number PHILOSOPHERS.
 % -> fork(Index, status)
-% -> philosopher(Index, Name, LeftForkPid, RightForkPid, action)
+% -> philosopher(Index, LeftForkPid, RightForkPid, action)
 %
 
 spawn_pair() ->
     RightFork = spawn(?MODULE, fork, [0, on_table]),
     LeftFork = spawn(?MODULE, fork, [1, on_table]),
     Name = get_name(0),
-    verbose(2, "Starting ~p~n", [Name]),
-    register(Name, spawn(?MODULE, philosopher, [0, Name, LeftFork, RightFork, thinking, 0])),
+    verbose(2, "Starting ~s~n", [capitalize(atom_to_list(Name))]),
+    register(Name, spawn(?MODULE, philosopher, [0, LeftFork, RightFork, thinking, 0])),
     % the left fork become the right fork for the next philosopher
     % and we keep the right fork to become the left fork of the last philosopher
     spawn_pair(1, LeftFork, RightFork).
@@ -70,56 +69,54 @@ spawn_pair(?PHILOSOPHERS, _, _) -> ok;
 
 spawn_pair(N, RightFork, FirstFork) ->
     Name = get_name(N),
-    verbose(2, "Starting ~p~n", [Name]),
+    verbose(2, "Starting ~s~n", [capitalize(atom_to_list(Name))]),
     case N + 1 =:= ?PHILOSOPHERS of
         % for the last philosopher, the left fork is the right fork of the
         % first philosopher
         true -> LeftFork = FirstFork;
         false -> LeftFork = spawn(?MODULE, fork, [N + 1, on_table])
     end,
-    register(Name, spawn(?MODULE, philosopher, [N, Name, LeftFork, RightFork, thinking, 0])),
+    register(Name, spawn(?MODULE, philosopher, [N, LeftFork, RightFork, thinking, 0])),
     % the left fork become the right fork for the next philosopher
     % and we keep the right fork to become the left fork of the last philosopher
-    spawn_pair(N + 1, LeftFork, RightFork).
+    spawn_pair(N + 1, LeftFork, FirstFork).
 
 %
 % report process
-% params: N, the number of rice eaten
 %
 
 capitalize([X|Xs]) when X >= $a orelse X =< $z -> [X - 32|Xs].
 
-kill_them_all(?PHILOSOPHERS) -> done;
-kill_them_all(N) ->
-    Pid = whereis(get_name(N)),
-    Pid ! please_kill_yourself_now,
-    kill_them_all(N + 1).
+exit_properly(?PHILOSOPHERS) -> done;
+exit_properly(N) ->
+    get_name(N) ! please_exit,
+    exit_properly(N + 1).
 
 report() ->
     receive
-        {philo, Name, eating, Cycle} ->
-            verbose(1, "[~w] ~s\t: ~s~n", [Cycle, capitalize(atom_to_list(Name)), atom_to_list(eating)]);
-        {philo, Name, thinking, Cycle} ->
-            verbose(1, "[~w] ~s\t: ~s~n", [Cycle, capitalize(atom_to_list(Name)), atom_to_list(thinking)]);
-        {philo, Name, hungry, Cycle} ->
-            verbose(1, "[~w] ~s\t: ~s~n", [Cycle, capitalize(atom_to_list(Name)), atom_to_list(hungry)]);
-        {philo, Name, pick_left, Cycle} ->
-            verbose(1, "[~w] ~s\t: picks up the left fork~n", [Cycle, capitalize(atom_to_list(Name))]);
-        {philo, Name, pick_right, Cycle} ->
-            verbose(1, "[~w] ~s\t: picks up the right fork~n", [Cycle, capitalize(atom_to_list(Name))]);
-        {philo, Name, drop_left, Cycle} ->
-            verbose(1, "[~w] ~s\t: puts down the left fork~n", [Cycle, capitalize(atom_to_list(Name))]);
-        {philo, Name, drop_right, Cycle} ->
-            verbose(1, "[~w] ~s\t: puts down the right fork~n", [Cycle, capitalize(atom_to_list(Name))]);
+        {philo, Idx, eating, Cycle} ->
+            verbose(2, "[~w] ~s\t: has the right and left fork~n", [Cycle, capitalize(atom_to_list(get_name(Idx)))]),
+            verbose(1, "[~w] ~s\t: ~s~n", [Cycle, capitalize(atom_to_list(get_name(Idx))), atom_to_list(eating)]);
+        {philo, Idx, thinking, Cycle} ->
+            verbose(1, "[~w] ~s\t: ~s~n", [Cycle, capitalize(atom_to_list(get_name(Idx))), atom_to_list(thinking)]);
+        {philo, Idx, hungry, Cycle} ->
+            verbose(1, "[~w] ~s\t: ~s~n", [Cycle, capitalize(atom_to_list(get_name(Idx))), atom_to_list(hungry)]);
+        {philo, Idx, pick_left, Cycle} ->
+            verbose(1, "[~w] ~s\t: picks up the left fork~n", [Cycle, capitalize(atom_to_list(get_name(Idx)))]);
+        {philo, Idx, pick_right, Cycle} ->
+            verbose(1, "[~w] ~s\t: picks up the right fork~n", [Cycle, capitalize(atom_to_list(get_name(Idx)))]);
+        {philo, Idx, drop_left, Cycle} ->
+            verbose(1, "[~w] ~s\t: puts down the left fork~n", [Cycle, capitalize(atom_to_list(get_name(Idx)))]);
+        {philo, Idx, drop_right, Cycle} ->
+            verbose(1, "[~w] ~s\t: puts down the right fork~n", [Cycle, capitalize(atom_to_list(get_name(Idx)))]);
         {fork, Pid, on_table} ->
             verbose(1, "Fork ~w\t: on table~n", [Pid]);
         {fork, Pid, in_use} ->
             verbose(1, "Fork ~w\t: in use~n", [Pid])
     after 2000 ->
         % in case of fool philosophers
-        % ask them to commit suicide with the forks
-        verbose(2, "Philosophers are stucked... kill everyone!!!", []),
-        kill_them_all(0),
+        verbose(2, "Philosophers are stucked... program is shutting down.", []),
+        exit_properly(0),
         exit(normal)
     end,
     report().
@@ -130,20 +127,18 @@ report() ->
 %
 
 fork(Pid, Status) ->
-    Report = whereis(report),
     receive
         {pick, Source} ->
             case Status =:= on_table of
                 true ->
                     Source ! ok,
-                    Report ! {fork, Pid, in_use};
+                    report ! {fork, Pid, in_use};
                 false ->
                     Source ! ko
             end,
             fork(Pid, in_use);
-        {drop, Source} ->
-            Source ! ok,
-            Report ! {fork, Pid, on_table},
+        drop ->
+            report ! {fork, Pid, on_table},
             fork(Pid, on_table)
     end.
 
@@ -151,43 +146,37 @@ fork(Pid, Status) ->
 % philosopher process
 %
 
-philosopher(Idx, Name, Left, Right, thinking, Cycle) ->
-    Report = whereis(report),
-    Report ! {philo, Name, thinking, Cycle},
-    timer:sleep(random:uniform(1000) + 100),
-    philosopher(Idx, Name, Left, Right, hungry, Cycle);
+philosopher(Idx, Left, Right, thinking, Cycle) ->
+    random:seed(erlang:phash2([node()]), erlang:monotonic_time(), erlang:unique_integer()),
+    report ! {philo, Idx, thinking, Cycle},
+    timer:sleep(random:uniform(1000 - 100 + 1) + 100),
+    philosopher(Idx, Left, Right, hungry, Cycle);
 
 % don't forget to drop the forks before being shutdown, because
 % there might be still hungry philosophers that wait for forks
-philosopher(Idx, Name, Left, Right, hungry, Cycle) ->
-    Report = whereis(report),
-    Report ! {philo, Name, hungry, Cycle},
-
-    % this trick avoid to have only one philosopher to eat
+philosopher(Idx, Left, Right, hungry, Cycle) ->
+    report ! {philo, Idx, hungry, Cycle},
+    % if the philosopher id mod 2 == 0 check left and right,
+    % otherwise check right and left
     case Idx rem 2 =:= 0 of
         true ->
-            verbose(2, "~n~s asks for the left fork~n", [Name]),
-            check_fork(Name, Left, left, Cycle),
-            verbose(2, "~n~s asks for the right fork~n", [Name]),
-            check_fork(Name, Right, right, Cycle);
+            check_fork(Idx, Left, left, Cycle),
+            check_fork(Idx, Right, right, Cycle);
         false ->
-            verbose(2, "~n~s asks for the right fork~n", [Name]),
-            check_fork(Name, Right, right, Cycle),
-            verbose(2, "~n~s asks for the left fork~n", [Name]),
-            check_fork(Name, Left, left, Cycle)
+            check_fork(Idx, Right, right, Cycle),
+            check_fork(Idx, Left, left, Cycle)
     end,
-    verbose(2, "~n~s has the right and left fork~n", [Name]),
-    philosopher(Idx, Name, Left, Right, eating, Cycle);
+    philosopher(Idx, Left, Right, eating, Cycle);
 
-philosopher(Idx, Name, Left, Right, eating, Cycle) ->
-    Report = whereis(report),
-    Report ! {philo, Name, eating, Cycle},
-    timer:sleep(random:uniform(1000) + 100),
-    drop_fork(Name, Left, Right, Cycle),
-    philosopher(Idx, Name, Left, Right, thinking, Cycle + 1).
+philosopher(Idx, Left, Right, eating, Cycle) ->
+    report ! {philo, Idx, eating, Cycle},
+    timer:sleep(random:uniform(1000 - 100 + 1) + 100),
+    drop_fork(Idx, Left, Right, Cycle),
+    philosopher(Idx, Left, Right, thinking, Cycle + 1).
 
-philosopher(Name, shutdown) ->
-    verbose(2, "~n~s kill himself... :'(~n", [Name]),
+% in case of deadlock
+philosopher(Idx, shutdown) ->
+    verbose(2, "~n~s exits... :'(~n", [capitalize(atom_to_list(get_name(Idx)))]),
     exit(normal).
 
 %
@@ -196,17 +185,19 @@ philosopher(Name, shutdown) ->
 % killhimself.
 %
 
-check_fork(Name, Fork, Direction, Cycle) ->
-    Report = whereis(report),
-    Fork ! {pick, whereis(Name)},
+check_fork(Idx, Fork, Direction, Cycle) ->
+    Fork ! {pick, get_name(Idx)},
+    verbose(2, "~n~s asks for the ~s fork~n", [capitalize(atom_to_list(get_name(Idx))), atom_to_list(Direction)]),
     receive
         ok ->
             case Direction =:= left of
-                true -> Report ! {philo, Name, pick_left, Cycle};
-                false -> Report ! {philo, Name, pick_right, Cycle}
+                % if the fork is on table, take the fork
+                true -> report ! {philo, Idx, pick_left, Cycle};
+                false -> report ! {philo, Idx, pick_right, Cycle}
             end;
-        ko -> check_fork(Name, Fork, Direction, Cycle);
-        please_kill_yourself_now -> philosopher(Name, shutdown)
+        % else ask again until the fork in on table
+        ko -> check_fork(Idx, Fork, Direction, Cycle);
+        please_exit -> philosopher(Idx, shutdown)
     end.
 
 %
@@ -214,18 +205,11 @@ check_fork(Name, Fork, Direction, Cycle) ->
 % this allow the other hungry philosophers to take them
 %
 
-drop_fork(Name, Left, Right, Cycle) ->
-    Report = whereis(report),
-    Pid = whereis(Name),
-    Left ! {drop, Pid},
-    receive
-        ok -> Report ! {philo, Name, drop_left, Cycle}
-    end,
-    Right ! {drop, Pid},
-    receive
-        ok -> Report ! {philo, Name, drop_right, Cycle}
-    end.
-
+drop_fork(Idx, Left, Right, Cycle) ->
+    Left ! drop,
+    report ! {philo, Idx, drop_left, Cycle},
+    Right ! drop,
+    report ! {philo, Idx, drop_right, Cycle}.
 
 %%
 %% CHALLENGE
@@ -255,20 +239,34 @@ drop_fork(Name, Left, Right, Cycle) ->
 % actor that send Foo ! {Pid, foo} or Bar ! {Pid, bar}, both processes will
 % be deadlocked.
 %
-% But there is no such case in this project because there is not two interdependent
+% But there is no such case in this project because there are not two interdependent
 % processes that are each waiting for a message from the other, but processes
-% which send a message and wait for an answer.
-% In the worst case, we could have infinite loop, like if a philosopher check
-% for the availability of a fork and the fork allways answer that it is unavailable.
-% Thus, the philosopher will ask again and again, but this is not a deadlock
-% strictly speaking. And even if this could happened in case of bad implementation,
-% the reporter will timeout after 2s without receiving any message and ask all the
-% philosophers to exit properly.
+% which send a message and wait for an answer. The design pattern is likely the
+% same as a client/server.
+% To understand this implementation, when a philosopher is spawn, he gets in parameters
+% the two forks pid linked to him, the left and the right. So we have to spawn two first forks
+% passed in parameter of the first philosopher and then the left fork become the right fork
+% of the next philosopher and a new fork is spawn to be his left fork and so on.
+% At the begining we keep the first right fork in memory to set it as the left fork of
+% the last philosopher.
+% In that way, each philosopher knows his two forks pid and could asks the forks is they are available
+% and then wait for a OK/KO answer from the forks. The forks don't send a
+% message first but are waiting for a message coming from the philosopher to
+% send them a response. And the reporter is just waiting for an incoming message
+% from the philosopher without sending response because its main purpose is to
+% report what happened, but philosopher's actions don't depend on the reporter.
+% In case of infinite loop, which might never occur, and so the reporter does not receive
+% any message from the philosopher during 2s, it will timeout and
+% send to all philosophers a message telling them to exit properly before
+% exiting itself.
 %
 % 2. Assuming that there is a danger of deadlock in the system, answer one of the
 % following questions: (a) How could the deadlock be avoided? (preventing it from
-% occuring in the first place)
+% occurring in the first place)
 %
 % The only rule to avoid deadlock is that if process A sends a message and waits
 % for a response from process B, process B is not allowed, to do a synchronous call
 % to process A, as the messages might cross and cause the deadlock.
+% But more generally, we could set a timeout, like for the reporter in case of
+% deadlock. Them, if a timeout is triggered the process terminates and if
+% we have linked the processes the termination will be propagated to them.
